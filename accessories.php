da7b79b0d8ee123c76dfca583d1553cee12bf5f1
icostaykov <icostaykov1@gmail.com>
2016-11-20 14:38:03 +0200

    sgsdf

---
+++ accessories.php
@@ -0,0 +1,25 @@
+<?php
+include 'views/head.php';
+include 'views/navigation.php';
+
+?>
+<div id="container-nav">
+	<div class="container" >
+		<div class="row">
+	               <div class="lower-nav"><a href="challanges.php">Challenges</a></div>
+	                <div class="lower-nav"><a href="rivals.php">Rivals</a></div>
+	                <div class="lower-nav"><a href="rates.php">Rate</a></div>
+	                <div class="lower-nav"><a href="accessories.php">Accessories</a></div>
+	                <div class="lower-nav"><a href="help.php">Help</a></div>
+	    </div>
+	</div>
+</div>	
+<div class="container">
+	<div class="row">
+			<div class="col-sm-12">
+		<img src="images/layout/aceup-16.png" alt="image" style="width: 960px; height: auto;" >
+			</div>
+	</div>
+</div>
+
+<?php include 'views/footer.php'; ?>
